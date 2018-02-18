4dcd6393b1ae9299b12536e097b679c69e06fa72
ivan <test>
2018-02-18 20:10:00 +0300

    m

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,26 +64,6 @@
                                 </div>
                             </div>
 
-                            <div class="form-group{{ $errors->has('captcha') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Captcha</label>
-                                <div class="col-md-6">
-                                    <div class="captcha">
-                                        <span>{!! captcha_img() !!}</span>
-                                        <button type="button" class="btn btn-success btn-refresh"><i
-                                                    class="fa fa-refresh"></i></button>
-                                    </div>
-
-                                    <input id="captcha" type="text" class="form-control" placeholder="Enter Captcha"
-                                           name="captcha">
-                                    @if ($errors->has('captcha'))
-                                        <span class="help-block">
-                                          <strong>{{ $errors->first('captcha') }}</strong>
-                                       </span>
-                                    @endif
-                                </div>
-                            </div>
-
-
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
